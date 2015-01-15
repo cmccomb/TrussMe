3dc5d081cf91b9026e46dba1e929959e322f0444
Chris McComb <ccmcc2012@gmail.com>
2015-01-14 19:37:43 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='pytruss',
+    name='trussme',
     version='0.0.1',
     description='Truss construction and analysis',
     long_description=readme,
     author='Christopher McComb',
     author_email='chris.c.mccomb@gmail.com',
-    url='https://github.com/cmccomb/pytruss',
+    url='https://github.com/cmccomb/truss-me',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
