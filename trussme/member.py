99fb49bf5f0202741d3d39ff5b1198cf356d3e02
cmccomb <ccmcc2012@gmail.com>
2015-03-27 09:51:06 -0400

    Beginning architecture for different member types.

--- trussme/member.py
+++ trussme/member.py
@@ -1,42 +1,69 @@
 import numpy
+import warnings
 
 class Member(object):
 
-    shape_names = ["pipe", "bar", "square", "box"]
+    # Shape types
+    shapes = ["pipe", "bar", "square", "box"]
 
-    def __init__(self):
+    # Material options and characteristics
+    #            name    rho   E               Fy
+    materials = {"A36": [7870, 200*pow(10, 9), 250*pow(10, 6)]}
+
+    def __init__(self, default=True):
         # Shape independent variables
-        self.t = 0.0   # thickness
-        self.w = 0.0   # outer width
-        self.h = 0.0   # outer height
-        self.r = 0.0   # outer radius
+        self.t = 0.0  # thickness
+        self.w = 0.0  # outer width
+        self.h = 0.0  # outer height
+        self.r = 0.0  # outer radius
 
         # Material properties
-        self.Fy = 0.0  # yield strength
-        self.rho = 0.0 # material density
+        self.material = ''  # string specifying material
+        self.E = 0.0        # Elastic modulus
+        self.Fy = 0.0       # yield strength
+        self.rho = 0.0      # material density
 
         # Dependent variables
         self.A = 0.0   # Cross-sectional area
         self.I = 0.0   # Moment of inertia
         self.LW = 0.0  # Linear weight
 
-        # Things that are started with default values
-        self.shape = "pipe"
-        self.material = ""
-        self.calc_properties()
+        # Set default values, and calculate properties
+        if default:
+            self.set_shape("pipe", update_props=False)
+            self.set_material("A36", update_props=False)
+            self.set_parameters(t=0.01, r=0.1, update_props=True)
 
-    def set_shape(self, new_shape):
+    def set_shape(self, new_shape, update_props=True):
+        # Read and save hte shape name
         if self.shape_name_is_ok(new_shape):
             self.shape = new_shape
-            self.calc_properties()
         else:
             raise ValueError(new_shape+' is not an defined shape. Try ' +
-                             ', '.join(self.shape_names[0:-1]) + ', or ' +
-                             self.shape_names[-1] + '.')
+                             ', '.join(self.shapes[0:-1]) + ', or ' +
+                             self.shapes[-1] + '.')
+
+        # If required, update properties
+        if update_props:
+            self.calc_properties()
 
 
-    def set_material(self, new_material):
-        asdf = 1
+    def set_material(self, new_material, update_props=True):
+        if self.material_name_is_ok(new_material):
+            self.material = new_material
+        else:
+            raise ValueError(new_material+' is not an defined shape. Try ' +
+                             ', '.join(self.materials.keys()[0:-1]) + ', or ' +
+                             self.materials.keys()[-1] + '.')
+
+        # Set material properties
+        self.rho = self.materials[new_material][0]
+        self.E = self.materials[new_material][1]
+        self.Fy = self.materials[new_material][2]
+
+        # If required, update properties
+        if update_props:
+            self.calc_properties()
 
     def set_parameters(self, **kwargs):
         # Save the values
@@ -57,18 +84,25 @@ class Member(object):
                 self.h = kwargs["height"]
             elif key is "h":
                 self.h = kwargs["h"]
+            elif kwargs["update_props"]:
+                self.calc_properties()
             else:
                 raise ValueError(key+' is not an defined shape. '
                                      'Try thickness (t), width (w), '
                                      'height (h), or radius (r).')
 
-
         # Check parameters
         if self.shape == "pipe":
-            if
+            if self.t > self.r:
+                warnings.warn("Thickness is greater than radius."
+                              "Changing shape to bar.")
         if self.shape == "box":
-
-
+            if 2*self.t > self.w:
+                warnings.warn("Thickness is greater than half of width."
+                              "Changing shape to square.")
+            elif 2*self.t > self.h:
+                warnings.warn("Thickness is greater than half of height."
+                              "Changing shape to square.")
 
     def calc_properties(self):
         # Calculate moment of inertia
@@ -78,10 +112,14 @@ class Member(object):
 
         # Calculate the
 
-
-
     def shape_name_is_ok(self, name):
-        if name in self.shape_names:
+        if name in self.shapes:
+            return True
+        else:
+            return False
+
+    def material_name_is_ok(self, name):
+        if name in self.materials.keys():
             return True
         else:
             return False
